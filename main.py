83384216a23b8e0bf226b82c354e9c3a01cef134
Prajaktha P Prabhu <72214495+prajakthaprabhu@users.noreply.github.com>
2022-09-02 19:11:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def count_battries_by_usage(cycles):
     "highCount"
   }
 for i in cycles:
-  if i<=400:
-    count["lowCount"] = count["lowCount"] +1
-    elif i >400 and i <=919 :
-      count["mediumCount"] = count["mediumCount"] + 1
-      else:
-        count["highCount"] = count["highCount"] + 1
-        return count
+ if i<=400:
+  count["lowCount"] = count["lowCount"] +1
+ elif i >400 and i <=919 :
+  count["mediumCount"] = count["mediumCount"] + 1
+ else:
+  count["highCount"] = count["highCount"] + 1
+return count
